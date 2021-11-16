0542fbacaba16329572cb3aeb35b8802ad84c0ed
yashica-k <93628137+yashica-k@users.noreply.github.com>
2021-11-16 17:47:17 -0500

    Update Ticket.java

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,29 +1,29 @@
 /*This is the ticket class. We will use this to display information that will usually be on a movie 
 ticket such as the name of the movie, the time, the date, theatre number and seat number. */
-//import java.util.*;
-//public class Ticket {
-//	
-//	//This is the theatre number
-//	theatreNumber t = new theatreNumber();
-//
-//	//This function will print out the ticket details
-//	void print_ticket(String movie,String showtime,int theatreNumber, String row, int col)
-//	{
-//		//This will show the date that the ticket is valid for
-//		Date date = new Date();
-//		//This would get the cost of the movie
-//		int cost = s.getcost(movie);
-//		System.out.println("========================SelfBook Cinemas=======================");
-//		System.out.println("\t\t      ----Ticket----");
-//		System.out.println("\tMovie: "+movie+"\t");
-//		System.out.print("\tTime: "+showtime);
-//		System.out.println("\t\t\tSeat: "+row+col);
-//		System.out.println("\t\t\tTheatre Number: "+theatreNumber);
-//		System.out.println("\tDate: "+date);
-//		System.out.println("\tPrice: $"+cost);
-//		System.out.println("\tThank You! We hope you enjoy the movie!\n\n");
-//	}
-//} 
-//	
-//	
-//}
+import java.util.*;
+public class Ticket {
+	
+	//This is the theatre number
+	theatreNumber t = new theatreNumber();
+
+	//This function will print out the ticket details
+	void print_ticket(String movie,String showtime,int theatreNumber, String row, int col)
+	{
+		//This will show the date that the ticket is valid for
+		Date date = new Date();
+		//This would get the cost of the movie
+		int cost = t.getcost(movie);
+		System.out.println("========================SelfBook Cinemas=======================");
+		System.out.println("\t\t      ----Ticket----");
+		System.out.println("\tMovie: "+movie+"\t");
+		System.out.print("\tTime: "+showtime);
+		System.out.println("\t\t\tSeat: "+row+col);
+		System.out.println("\t\t\tTheatre Number: "+theatreNumber);
+		System.out.println("\tDate: "+date);
+		System.out.println("\tPrice: $"+cost);
+		System.out.println("\tThank You! We hope you enjoy the movie!\n\n");
+	}
+} 
+	
+	
+}
