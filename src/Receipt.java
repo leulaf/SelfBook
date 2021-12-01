b27708221c967feef1acc055cdbff8ca7c02b146
Sueda <snalcaci@bu.edu>
2021-11-30 21:21:31 -0500

    added receipt class

---
+++ src/Receipt.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Receipt {
+
+    private int numSeats;
+    private double totalPaid;
+    ArrayList<Seat> seats;
+    private int theaterNumber;
+    private String movieName;
+
+    
+    public Receipt(Movie movie, ArrayList<Seat> s, int numSeats, int theaterNum){
+        this.totalPaid = movie.getPrice() * numSeats;
+        this.movieName = movie.getName();
+        this.numSeats = numSeats;
+        this.theaterNumber = theaterNum;
+        seats = new ArrayList<Seat>(s);
+        
+    }
+    //printing an easy to read receipt
+    public String toString(){
+        
+        String s = " Please print this page for future reference. \n";
+        s += "----------------\n" ;
+        s += "   Receipt   \n";
+        s += "Date: " + java.time.LocalDate.now() + "\n"; 
+        s += "Movie Name: " + this.movieName + "\n"; 
+        s += "Theater Number:" + this.theaterNumber + "\n";
+        s += "Seat Numbers: " ;
+        for (Seat seat : this.seats)
+            s+= seat + " ";
+        s += "\n";
+        s += "Total Paid "+ this.totalPaid;
+        s += "   ---------    ";
+
+        return s;
+
+
+        
+    }
+
+
+    
+
+    
+}
