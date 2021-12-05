3be319fbe76785caa609cecacdde4376c520db89
heatappadmin <78217479+heatappadmin@users.noreply.github.com>
2021-12-05 17:32:09 -0500

    bug fix

--- src/Admin.java
+++ src/Admin.java
@@ -79,7 +79,7 @@ public class Admin {
 		Time timeGiven = new Time();
 		boolean timeValid = timeGiven.parseTime(time);
 		if (!timeValid) {
-			System.out.println("The time range entered was not valid. Please try again!");
+			// fail so return false
 			return false;
 		}
 		
@@ -91,6 +91,7 @@ public class Admin {
 			for (Map.Entry<Time, Theatre> entry : currMovie.timeLocation.entrySet()) {
 				Time currTime = entry.getKey();
 				Theatre currTheatre = entry.getValue();
+				// check that the movie is equal
 				if (currTheatre.equals(location)) {
 					// now we need to check for time overlap
 					boolean hasOverlap = timeGiven.checkOverlap(currTime);
